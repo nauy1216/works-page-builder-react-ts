1f5874dbe2394562dae840ab4c27a3132bdf060b
liuchengyuan <liucy@rp-pet.cn>
2021-04-18 16:31:06 +0800

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
+import { RouteConfigComponentProps } from 'react-router-config';
 import { Button } from 'antd';
 import { useDocTitle } from '../../hooks';
 
@@ -18,10 +19,10 @@ function A(props: any) {
   );
 }
 
-const Home: React.FC<any> = routes => {
+const Home: React.FC<RouteConfigComponentProps> = routes => {
   console.log('routes', routes);
   const { route } = routes; // 获取传入的路由配置
-  useDocTitle(route.title); // 修改标题
+  useDocTitle(route?.title); // 修改标题
   return (
     <div>
       <A></A>
